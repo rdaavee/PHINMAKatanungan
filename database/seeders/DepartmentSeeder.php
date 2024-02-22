4345c2fc04a873c4d6e012feb2ea04851eade508
Danielle Marie <daniellemariedeguzman@gmail.com>
2024-02-23 00:19:37 +0800

    table seeders

--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -13,9 +13,35 @@ class DepartmentSeeder extends Seeder
      */
     public function run(): void
     {
-        Department::create([
-            'department_id' => 'CITE',
-            'name' => 'College of Information Technology Education'
-        ]);
+        $departments = [
+            [
+                'department_id' => 'CITE',
+                'name' => 'College of Information Technology Education',
+            ],
+            [
+                'department_id' => 'CEA',
+                'name' => 'College of Engineering and Architecture',
+            ],
+            [
+                'department_id' => 'CAHS',
+                'name' => 'College of Allied Health Sciences',
+            ],
+            [
+                'department_id' => 'CCJE',
+                'name' => 'College of Criminal Justice Education',
+            ],
+            [
+                'department_id' => 'CELA',
+                'name' => 'College of Education and Liberal Arts',
+            ],
+            [
+                'department_id' => 'CMA',
+                'name' => 'College of Management and Accountancy',
+            ],
+        ];
+
+        foreach ($departments as $departmentData) {
+            Department::create($departmentData);
+        }
     }
 }
