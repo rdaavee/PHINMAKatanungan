50ba5a52a67eb1764f45154b59184bf8367fef7c
tally-sore <colladojean15@gmail.com>
2024-02-23 13:56:34 +0800

    test3

--- routes/api.php
+++ routes/api.php
@@ -26,8 +26,8 @@ Route::middleware('auth:teacher')->get('/teacher', function (Request $request) {
 });
 
 Route::put('/users/{user_id}', 'UserController@update');
-Route::post('/api/store', 'UserMobileController@store');
-Route::post('/api/createteacher', [UserMobileController::class, 'storeTeacher'])->withoutMiddleware('auth:sanctum');
+Route::post('/store', 'UserMobileController@store');
+Route::post('/createteacher', [UserMobileController::class, 'storeTeacher'])->withoutMiddleware('auth:sanctum');
 
 Route::post('/userlogin', [UserMobileController::class, 'userLogin'])->withoutMiddleware('auth:sanctum');
 
