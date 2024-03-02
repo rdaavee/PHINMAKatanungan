cf86982263032d1036f5f8197a9016a0e6ab9de2
tally-sore <maeldoesvalo@gmail.com>
2024-03-02 17:55:32 +0800

    updated postmobilecontroller

--- app/Http/Controllers/Mobile/PostsMobileController.php
+++ app/Http/Controllers/Mobile/PostsMobileController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Mobile;
 
+use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -22,19 +23,30 @@ class PostsMobileController extends Controller
 
         $requestData = [
             'user_id'=> $request->user_id,
+            'first_name' => $request->first_name,
             'title' => $request->title,
             'content'=> $request->content,
             'privacy'=> $request->privacy,
         ];
 
+        $posts = Post::with('user:user_id,first_name')->get();
 
         try {
             $user = Post::create($requestData);
             Log::info('Post Created Successfully');
-            return response()->json(['message' => 'Post successfully created.'], 200);
+            return response()->json(['message' => 'Post successfully created.', 'posts' => $posts], 200);
         } catch (\Exception $e) {
             Log::error('Error creating post record: ' . $e->getMessage());
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
+
+    
+    public function getPosts()
+    {
+        $posts = Post::with('user:user_id,first_name,middle_name,last_name,course_id')->get();
+
+        return response()->json(['posts' => $posts], 200);
+    }
+    
 }
