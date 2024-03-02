a44bff92a4246e1881bfd999253f8d780164cec4
tally-sore <maeldoesvalo@gmail.com>
2024-03-02 17:55:06 +0800

    updated usermobilecontroller

--- app/Http/Controllers/Mobile/UserMobileController.php
+++ app/Http/Controllers/Mobile/UserMobileController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Mobile;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Student;
 use App\Models\Teacher;
